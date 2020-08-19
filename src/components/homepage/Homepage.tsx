d26410dcc4e478d107bb721476616a4aa3ce28d9
Jessica <jessica@spoonity.com>
2020-08-19 18:43:06 -0400

    start repo

---
+++ src/components/homepage/Homepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Homepage.css';
+
+export class Homepage extends React.Component {
+  render() {
+    return (
+      <div className="font--main homepage-wrapper">Homepage</div>
+    )
+  }
+}
