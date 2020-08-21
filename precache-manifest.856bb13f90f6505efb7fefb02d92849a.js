bdb510291f4d6bf646059500db562edcbb9219af
Jessica Antonious <jessica@prieto.ca>
2020-08-21 13:57:15 -0400

    Updates

---
+++ precache-manifest.856bb13f90f6505efb7fefb02d92849a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6161fd47bed083ab06e9c0634d2e7387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78ca400e3aefe85f65f5",
+    "url": "/static/css/main.794d28cf.chunk.css"
+  },
+  {
+    "revision": "47050964278b727537be",
+    "url": "/static/js/2.e09c5cb4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e09c5cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ca400e3aefe85f65f5",
+    "url": "/static/js/main.6f17f80f.chunk.js"
+  },
+  {
+    "revision": "e279cb453be9c26fed4f",
+    "url": "/static/js/runtime-main.56a77e7f.js"
+  }
+]);
